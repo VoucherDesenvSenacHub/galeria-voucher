15280bd9a43739daf1986d84fb3f82050c7c3c81
José Otávio Alves <joseotavioalves321@gmail.com>
2025-04-07 19:38:23 -0400

    teste 2

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
+require_once __DIR__ . '/APP/config/env.php';
 
-require_once __DIR__ . '\APP\view\pages\users\home.php';
+header('Location: ' . APP_CONSTANTS['APP_URL'] . APP_CONSTANTS['DIR_USER'] . 'home.php');
