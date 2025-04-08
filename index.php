e6fb7ce6990222497412e1014ff3319bb1eb3e23
José Otávio Alves <joseotavioalves321@gmail.com>
2025-04-07 20:03:15 -0400

    variaveis

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 require_once __DIR__ . '/APP/config/env.php';
 
-header('Location: ' . APP_CONSTANTS['APP_URL'] . APP_CONSTANTS['DIR_USER'] . 'home.php');
+header('Location: ' . VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_USER'] . 'home.php');
