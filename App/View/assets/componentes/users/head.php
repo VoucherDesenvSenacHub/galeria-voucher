3d4e6bf0f123ab21ebd4cc03f0dbb87f9ce82f1f
José Otávio Alves <joseotavioalves321@gmail.com>
2025-04-07 21:12:29 -0400

    reorganização

--- App/View/assets/componentes/users/head.php
+++ App/View/assets/componentes/users/head.php
@@ -9,7 +9,7 @@ require_once __DIR__ . "\.\..\..\..\..\Config/env.php"
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Galeria Voucher</title>
 
-    <link rel="stylesheet" href="<?= VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_CSS'] ?>users/styles.css">
+    <link rel="stylesheet" href="<?= VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_CSS'] ?>globals.css">
 </head>
 <body>
     <h1>Hello World</h1>
