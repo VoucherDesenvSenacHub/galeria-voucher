4ec619f74854e6f33abcd9946b0ce2c58fabf7f9
José Otávio Alves <joseotavioalves321@gmail.com>
2025-04-09 21:08:26 -0400

    manutenutenção do env e links

--- App/View/pages/users/home.php
+++ App/View/pages/users/home.php
@@ -1,2 +1,5 @@
 <?php require_once __DIR__ . "/./../../componentes/head.php" ?>
+
+
+<?php require_once __DIR__ . "/./../../componentes/users/mira.php" ?>
 <?php require_once __DIR__ . "/./../../componentes/users/footer.php" ?>
