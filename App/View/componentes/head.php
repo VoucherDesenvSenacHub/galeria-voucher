d2ccf8acaf255678964e546ef6868a3ba0054b5e
Carlos Eduardo <carlosborges2007edu@gmail.com>
2025-04-11 21:11:17 -0400

    .

--- App/View/componentes/head.php
+++ App/View/componentes/head.php
@@ -9,7 +9,6 @@ require_once __DIR__ . '/./../../Config/env.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Galeria Voucher</title>
 
     <!-- fonte Poppins -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
