090aa3a97ae27279566ab7ec638eb4351c20a8e6
Gustaaa1231 <guguimaraes13@gmail.com>
2025-04-12 19:41:07 -0300

    fix: erro de diretorio das imagens e head.php

--- App/View/componentes/users/card_desenvolvedores.php
+++ App/View/componentes/users/card_desenvolvedores.php
@@ -1,7 +1,6 @@
 <?php 
-    include("./head.php");
+    require_once "../head.php";
 ?>
-
 <head>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -9,7 +8,7 @@
 </head>
 <div class="card-container">
                 <div class="image-container">
-                    <img src="../../img/utilitarios/img_card_dev_exemplo.jpg" alt="Foto de Perfil">
+                    <img src="../../assets/img/utilitarios/img_card_dev_exemplo.jpg" alt="Foto de Perfil">
                 </div>
                 <div class="text-card">
                     <h2>EDNALDO PEREIRA</h2>
