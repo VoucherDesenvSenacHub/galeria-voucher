e747a74286d9873b432ebd07b084c38af3a72181
RiquelmeG22 <riquelmeramalho762@gmail.com>
2025-04-11 20:18:42 -0400

    criando componente sidebar

---
+++ App/View/componentes/adm/sidebar.php
@@ -0,0 +1,10 @@
+
+<aside class="sidebar">
+  <ul class="menu">
+    <li><a href="#">USUARIOS</a></li>
+    <li><a href="#">TURMAS</a></li>
+    <li>
+      <a href="#">ESTATÍSTICAS <i class="icon-lock"></i></a>
+    </li>
+  </ul>
+</aside>
