cf5e2f7a7a177b61e622126235db629952fc8914
OtavioDayrots <152044189+OtavioDayrots@users.noreply.github.com>
2025-04-13 21:22:10 -0400

    css do body

--- App/View/pages/users/home.php
+++ App/View/pages/users/home.php
@@ -1,5 +1,8 @@
 <?php require_once __DIR__ . "/./../../componentes/head.php" ?>
 
+<body>
+    <?php require_once __DIR__ . "/./../../componentes/users/mira.php" ?>
+    <?php require_once __DIR__ . "/./../../componentes/users/footer.php" ?>
 
-<?php require_once __DIR__ . "/./../../componentes/users/mira.php" ?>
-<?php require_once __DIR__ . "/./../../componentes/users/footer.php" ?>
+</body>
+</html>
