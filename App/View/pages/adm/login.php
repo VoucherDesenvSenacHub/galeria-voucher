74368158a0be4bd8a55ee9ea55745fcb14e71ce5
Manoel V. L. de Oliveira <140553447+Tewdric@users.noreply.github.com>
2025-04-14 21:51:57 -0400

    subindo

---
+++ App/View/pages/adm/login.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<?php 
+   require_once(__DIR__ . '/../../assets/componentes/users/head.php');
+?>
+
+<body>
+    <header>
+        <nav>
+            <img src="assets/voucher-logo.png" width="150px">
+            <div class="search">
+                <div class="search">
+                    <input type="text" placeholder="Pesquisar">
+                    <button>&#128269;</button>
+                </div>
+            </div>
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">TURMAS</a></li>
+                <li> <a href="#">
+                        <span class="material-symbols-outlined">
+                            person
+                        </span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="content">
+    
+        <div class="square"></div>
+
+            
+            
+        <div class="square1"></div>
+
+            
+        
+        <div class="square2"></div>
+
+    <main>
+        <form  class="form" action="">
+
+            <div class="form-header" action="">
+                <h1>Login</h1>
+
+            </div>
+                
+            <div class="form-content">
+
+                <div class= "form-input required">
+
+                    <input  type="email"  placeholder="Email" required>
+                    <input type="password" placeholder="Senha" required>
+        
+                </div>
+
+                <div class="form-action">
+
+                    <button type="submit"> Login </button>
+
+                </div>
+
+                <div class="form-footer">
+
+                    <p>Esqueceu sua senha ? <a href=""></a> Clique Aqui</p>
+
+                </div>
+
+            </div>
+          
+        </form>
+               
+    </main>
+    
+    <footer class="rodape">
+        <p>&copy; 2024 VOUCHER DESENVOLVEDOR 146 </p>
+    </footer>
+
+<body>
+
+</html>
