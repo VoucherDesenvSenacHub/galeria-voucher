02e651299f9063fe13a51a72f397b8f51595e23f
jeipe <jp.2013barbosa@gmail.com>
2025-04-15 19:25:24 -0400

    commit
    imagens

--- App/View/pages/users/turma.php
+++ App/View/pages/users/turma.php
@@ -25,109 +25,109 @@ require_once __DIR__ . "/../../componentes/head.php";
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 130</h3>
-                    <img src="imagens/templates/foto.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
 
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 131</h3>
-                    <img src="imagens/templates/foto1.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
 
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 132</h3>
-                    <img src="imagens/templates/foto3.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
 
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 133</h3>
-                    <img src="imagens/templates/foto2.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 134</h3>
-                    <img src="imagens/templates/foto1.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
 
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 135</h3>
-                    <img src="imagens/templates/foto.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
             
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 136</h3>
-                    <img src="imagens/templates/foto.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
 
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 137</h3>
-                    <img src="imagens/templates/foto1.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
 
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 138</h3>
-                    <img src="imagens/templates/foto3.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
 
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 137</h3>
-                    <img src="imagens/templates/foto2.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 139</h3>
-                    <img src="imagens/templates/foto1.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
 
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 140</h3>
-                    <img src="imagens/templates/foto.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
             
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 141</h3>
-                    <img src="imagens/templates/foto.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
            
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 142</h3>
-                    <img src="imagens/templates/foto.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 143</h3>
-                    <img src="imagens/templates/foto1.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
 
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 144</h3>
-                    <img src="imagens/templates/foto.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
 
@@ -137,74 +137,74 @@ require_once __DIR__ . "/../../componentes/head.php";
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 145</h3>
-                    <img src="imagens/templates/foto1.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 146</h3>
-                    <img src="imagens/templates/foto.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 147</h3>
-                    <img src="imagens/templates/foto.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 148</h3>
-                    <img src="imagens/templates/foto.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
             
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 149</h3>
-                    <img src="imagens/templates/foto1.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 150</h3>
-                    <img src="imagens/templates/foto.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 151</h3>
-                    <img src="imagens/templates/foto1.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 152</h3>
-                    <img src="imagens/templates/foto.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 153</h3>
-                    <img src="imagens/templates/foto1.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 154</h3>
-                    <img src="imagens/templates/foto.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 155</h3>
-                    <img src="imagens/templates/foto1.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
             <a href="emdesenvolvimento.html" class="card">
                 <div class="card-content">
                     <h3 class="card-title">TURMA 156</h3>
-                    <img src="imagens/templates/foto.jpg" alt="Turma 149" class="card-image">
+                    <img class="card-image" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma.jpg">
                 </div>
             </a>
             
