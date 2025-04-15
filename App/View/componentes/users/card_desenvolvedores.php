72d531095834023de1e5d6ed503250f3a008f55c
Manoel V. L. de Oliveira <140553447+Tewdric@users.noreply.github.com>
2025-04-14 20:08:00 -0400

    Subindo update

--- App/View/componentes/users/card_desenvolvedores.php
+++ App/View/componentes/users/card_desenvolvedores.php
@@ -1,11 +1,7 @@
 <?php 
     require_once "../head.php";
 ?>
-<head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-</head>
+
 <div class="card-container">
                 <div class="image-container">
                     <img src="../../assets/img/utilitarios/img_card_dev_exemplo.jpg" alt="Foto de Perfil">
