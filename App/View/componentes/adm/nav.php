5d298b021c3fb6973a0287cbbbf0d272a29363a5
RiquelmeG22 <riquelmeramalho762@gmail.com>
2025-04-14 21:58:23 -0400

    criando páginas

---
+++ App/View/componentes/adm/nav.php
@@ -0,0 +1,20 @@
+<?php 
+require_once __DIR__ . "\.\..\head.php"
+?>
+
+<header class="header-adm"> 
+    <nav class="nav-adm">
+        <div class="imgvoucher">
+        <img src="../voucher-logo.png" width="150px" />
+        </div>
+        <ul>
+        <li><a href="">HOME</a></li>
+        <li><a href="">TURMAS</a></li>
+        <li>
+            <a href="" class="botao-person">
+            <span class="material-symbols-outlined">person</span>
+            </a>
+        </li>
+        </ul>
+    </nav>
+</header>
---
+++ App/View/pages/adm/home.php
@@ -0,0 +1,25 @@
+<?php 
+require_once __DIR__ . "/../../../Config/env.php";
+require_once __DIR__ . "/../../componentes/head.php";
+?>
+
+<body>
+
+    <?php require_once __DIR__ . "/./../../componentes/adm/nav.php"; ?>
+    <?php require_once __DIR__ . "/./../../componentes/adm/sidebar.php"; ?>
+
+    <main class="content">
+        <div class="user-profile">
+        <div class="user-icon"></div>
+        <div class="user-name">NOME DO USUÁRIO</div>
+        </div>
+        <div class="welcome-message">BEM VINDO</div>
+
+        <style>
+        main {
+
+            background-color: #fff;
+        } 
+        </style>
+    </main>
+</body> 
