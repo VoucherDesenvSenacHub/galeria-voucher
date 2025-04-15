59667c3a63e2130fe1009924acce3bac30c8d71a
Samuel Serri <samuelcerri@gmail.com>
2025-04-15 19:47:16 -0400

    feito input e css

--- App/View/componentes/input.php
+++ App/View/componentes/input.php
@@ -5,14 +5,14 @@
  * @param string $type    Define o tipo do input ('text', 'password'). adicionaremos mais depois.
  * @param string $label  titulo do input (texto) que será exibido em cima do input.
  * @param string $name (texto) Define o name do input.
- * @param string $placehoder (Opcional) define um texto dentro do input.
+ * @param string $placeholder (Opcional) define um texto dentro do input.
  *
  * Exemplo de uso:
  * inputComponent('text', 'usuario', 'login_user', 'escreva seu usuario aqui'(opcional));                    
  * inputComponent('password', 'senha', 'login_senha','escreva sua senha aqui'(opcional) );           
  
  */
-function inputComponent($type, $label, $name, $placehoder = null) {
+function inputComponent($type, $label, $name, $placeholder = null) {
     // Define a classe CSS com base no estilo informado
     if ($type === 'text') {
         $class = 'input-text';
@@ -29,7 +29,7 @@ function inputComponent($type, $label, $name, $placehoder = null) {
    echo 
     "<div class = 'input-container'>
         <label for='$name'>$label:</label><br>
-        <input type='$type' class='$class' name='$name'><br>
-    </div>"
-}
+        <input type='$type' class='$class' name='$name' placeholder = '$placeholder'><br>
+    </div>";
+    }
 ?>
--- App/View/pages/users/home.php
+++ App/View/pages/users/home.php
@@ -3,3 +3,7 @@
 
 <?php require_once __DIR__ . "/./../../componentes/users/mira.php" ?>
 <?php require_once __DIR__ . "/./../../componentes/users/footer.php" ?>
+
+<?php inputComponent("text", "Usuario", "usuario", "escreva aqui");?>
+<?php inputComponent("password", "Senha", "senha", "escreva aqui");?>
+
