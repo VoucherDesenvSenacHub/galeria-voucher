158f79c0a40b607e2c892f76e5660294dd68e20a
Matheus da Silva Corsine <matheuscorsine4129@gmail.com>
2025-04-17 20:03:43 -0400

    manutenção da página inicial

--- App/View/pages/users/home.php
+++ App/View/pages/users/home.php
@@ -13,15 +13,15 @@ require_once __DIR__ . "/../../componentes/head.php";
 
                 <div class="content">
                     <div class="numero">
-                        <img src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>numeros.png" alt="Números">
+                        <img src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>utilitarios/numeros.png" alt="Números">
                     </div>
 
-                    <div class="nome">
-                        <img src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>nome.png" alt="Voucher Desenvolvedor">
+                    <div class="nome-voucher">
+                        <img src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>utilitarios/nome.png" alt="Voucher Desenvolvedor">
                     </div>
 
                     <div class="mapa">
-                        <img src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>mapa.png" alt="Mapa do Brasil">
+                        <img src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>utilitarios/mapa.png" alt="Mapa do Brasil">
                     </div>
                 </div>
             </section>
@@ -93,12 +93,23 @@ require_once __DIR__ . "/../../componentes/head.php";
             </section>
 
             <!-- Seção 4 (transição da página inicial para a página de "turmas" e animação dos losângos) -->
-            <section>
+            <section id="secao4">
                 
                 <div class="call-to-action">
-                    VEJA VÁRIOS PROJETOS E <span>INSPIRE-SE</span>
+                    <p>SELECIONE UMA TURMA E <span>INSPIRE-SE</span></p>
                 </div>
 
+                <div class="poligono">
+
+                    <div class="image-row">
+                        <?php for($i=0; $i<=5; $i++){ ?>
+                            <div class='image-container'>
+                                <img src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>utilitarios/foto.png" >
+                            </div>
+                        <?php } ?>
+                    </div>
+
+                    <div class="image-row">
                         <?php for($i=0; $i<=4; $i++){ ?>
                             <div class='image-container'>
                                 <img src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>utilitarios/foto.png" >
