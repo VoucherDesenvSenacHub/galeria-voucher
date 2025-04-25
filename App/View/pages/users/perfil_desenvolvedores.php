a33494799f462ef0d76cc417685dd800c2fff342
Samuel Serri <samuelcerri@gmail.com>
2025-04-25 19:38:56 -0400

    ajuste leve

--- App/View/pages/users/perfil_desenvolvedores.php
+++ App/View/pages/users/perfil_desenvolvedores.php
@@ -1,39 +1,37 @@
 <?php 
-// require_once __DIR__ . "/../../../Config/env.php";
+require_once __DIR__ . "/../../../Config/env.php";
 require_once __DIR__ . "/../../componentes/head.php";
+
+
+
+
+
+
 ?>
-
-
 <body class="body_dev">
     <header class="header_dev">
         <button href="#" class="voltar">VOLTAR</button>
         <div class="titulo-pagina">
-        <h1 class="titulodev">DESENVOLVEDORES</h1>
+            <h1 class="titulodev">DESENVOLVEDORES</h1>
         </div>
     </header>
     <main class="main_dev">
     <?php require_once __DIR__ . "/./../../componentes/users/mira.php"; //componente da mira ?>
         <div class="container">
+       <?php require_once __DIR__ . "/../../componentes/head.php";?>
             <?php
             // Defina o número de cards que você quer exibir
             $numeroDeCards = 16;
 
-    <main class="main_dev">
-        <?php require_once __DIR__ . "/./../../componentes/users/mira.php"; //componente da mira ?>
-            
-            <div class="container">
-                <?php
-                // Defina o número de cards que você quer exibir
-                $numeroDeCards = 16;
-
-                for ($i = 0; $i < $numeroDeCards; $i++) {
-                    require __DIR__ . "/../../../View/componentes/users/card_desenvolvedores.php";
-                }
-                ?>
-            </div>
+            for ($i = 0; $i < $numeroDeCards; $i++) {
+                require __DIR__ . "/../../../View/componentes/users/card_desenvolvedores.php";
+            }
+            ?>
+        </div>
     </main>
 
-    <?php require_once __DIR__ . "/./../../componentes/users/footer.php" //componente do rodapé ?>
-
+    <div class="textofinal">
+    <?php require_once __DIR__ . "/./../../componentes/users/footer.php"; ?>
+    </div>
 </body>
 </html>
