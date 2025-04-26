14088f24459d9aef25f6dbd22bec74711293b591
Rodrigo Santos <rodrigorrs.sp@gmail.com>
2025-04-25 21:23:10 -0400

    comentarios

--- App/View/pages/users/perfil_desenvolvedores.php
+++ App/View/pages/users/perfil_desenvolvedores.php
@@ -1,16 +1,12 @@
 <?php 
-require_once __DIR__ . "/../../../Config/env.php";
-require_once __DIR__ . "/../../componentes/head.php"; 
+require_once __DIR__ . "/../../../Config/env.php"; // 
+require_once __DIR__ . "/../../componentes/head.php"; // importa as fontes
 
-
-
-
-
-
+// botão voltar
 ?>
 <body class="body_dev">
     <header class="header_dev">
-    <?php buttonComponent('primary', 'VOLTAR'); ?>
+    <?php buttonComponent('primary', 'VOLTAR'); ?> 
         <div class="titulo-pagina">
             <h1 class="titulodev">DESENVOLVEDORES</h1>
         </div>
@@ -18,20 +14,20 @@ require_once __DIR__ . "/../../componentes/head.php";
     <main class="main_dev">
     <?php require_once __DIR__ . "/./../../componentes/users/mira.php"; //componente da mira ?>
         <div class="container">
-       <?php require_once __DIR__ . "/../../componentes/head.php";?>
+            <?php require_once __DIR__ . "/../../componentes/head.php";?> 
             <?php
-            // Defina o número de cards que você quer exibir
-            $numeroDeCards = 16;
+            
+            $numeroDeCards = 16; // Define o número de cards que você quer exibir
 
-            for ($i = 0; $i < $numeroDeCards; $i++) {
-                require __DIR__ . "/../../../View/componentes/users/card_desenvolvedores.php";
+            for ($i = 0; $i < $numeroDeCards; $i++) { 
+                require __DIR__ . "/../../../View/componentes/users/card_desenvolvedores.php"; // importa os cards
             }
             ?>
         </div>
     </main>
-
     <div class="textofinal">
-    <?php require_once __DIR__ . "/./../../componentes/users/footer.php"; ?>
-    </div>
+    <?php require_once __DIR__ . "/./../../componentes/users/footer.php"; // rodapé ?> 
+    </div> 
+    
 </body>
 </html>
