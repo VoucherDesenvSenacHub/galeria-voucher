4f21e251b8518c5ae81d6c66f7c7b76319033051
Luiz Henrique <lhso1991@gmail.com>
2025-04-25 20:38:07 -0400

    Feat: atualizando

---
+++ App/View/pages/adm/cadastroTurmas/cadastroTurmas.php
@@ -0,0 +1,36 @@
+<?php 
+require_once __DIR__ . "/../../../../Config/env.php";
+require_once __DIR__ . "/../../../componentes/head.php";
+?>
+
+  <body class="body-adm">
+    <div class="container-adm">
+       
+        <?php require_once __DIR__ . "/./../../../componentes/adm/sidebar.php"; ?>
+        <?php require_once __DIR__ . "/./../../../componentes/adm/nav.php"; ?>
+
+        <main class="main-turmas-turmas">
+            <div class="tabs">
+                <button class="tab active">TURMA</button>
+                <button class="tab">SOBRE</button>
+                <button class="tab">IMAGENS</button>
+            </div>
+
+            <div class="container">
+                <div class="form-section">
+                <h1>CADASTRO</h1>
+                <input type="text" placeholder="Nome:" />
+                <input type="text" placeholder="Ano da Turma:" />
+                <input type="text" placeholder="Polo:" />
+                <input type="text" placeholder="Docentes:" />
+                <input type="text" placeholder="Alunos:" />
+                </div>
+
+                <div class="profile-pic">
+                <img src="" alt="Ícone de usuário" />
+                </div>
+            </div>
+        </main> 
+    </div> 
+  </body>
+</html>
--- App/View/pages/adm/home-adm.php
+++ App/View/pages/adm/home-adm.php
@@ -9,7 +9,7 @@ require_once __DIR__ . "/../../componentes/head.php";
         <?php require_once __DIR__ . "/./../../componentes/adm/sidebar.php"; ?>
         <?php require_once __DIR__ . "/./../../componentes/adm/nav.php"; ?>
 
-      <!-- <main class="content-adm">
+      <main class="content-adm">
           <div class="user-profile-adm">
               <div class="user-icon-adm"></div>
 
@@ -21,31 +21,7 @@ require_once __DIR__ . "/../../componentes/head.php";
           <div class="welcome-message-adm">
               BEM VINDO
           </div>
-      </main> -->
-
-
-      <main class="main-turmas-turmas">
-            <div class="tabs">
-              <button class="tab active">TURMA</button>
-              <button class="tab">SOBRE</button>
-              <button class="tab">IMAGENS</button>
-            </div>
-
-            <div class="container">
-              <div class="form-section">
-                <h1>CADASTRO</h1>
-                <input type="text" placeholder="Nome:" />
-                <input type="text" placeholder="Ano da Turma:" />
-                <input type="text" placeholder="Polo:" />
-                <input type="text" placeholder="Docentes:" />
-                <input type="text" placeholder="Alunos:" />
-              </div>
-
-              <div class="profile-pic">
-                <img src="" alt="Ícone de usuário" />
-              </div>
-            </div>
-          </main> 
+      </main> 
     </div> 
   </body>
 </html>
