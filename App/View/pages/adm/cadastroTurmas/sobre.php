af5027595bdbc461a203dbf2e01cd1fb6aafca00
RiquelmeG22 <riquelmeramalho762@gmail.com>
2025-04-25 21:46:47 -0400

    ok

--- App/View/pages/adm/cadastroTurmas/sobre.php
+++ App/View/pages/adm/cadastroTurmas/sobre.php
@@ -8,29 +8,31 @@ require_once __DIR__ . "/../../../componentes/head.php";
        
         <?php require_once __DIR__ . "/./../../../componentes/adm/sidebar.php"; ?>
         <?php require_once __DIR__ . "/./../../../componentes/adm/nav.php"; ?>
-<!-- 
+
         <main class="main-turmas-turmas">
             <div class="tabs">
-                <button class="tab active">TURMA</button>
-                <button class="tab">SOBRE</button>
+                <button class="tab">TURMA</button>
+                <button class="tab active">SOBRE</button>
                 <button class="tab">IMAGENS</button>
             </div>
 
-            <div class="container">
-                <div class="form-section">
-                <h1>CADASTRO</h1>
-                <input type="text" placeholder="Nome:" />
-                <input type="text" placeholder="Ano da Turma:" />
-                <input type="text" placeholder="Polo:" />
-                <input type="text" placeholder="Docentes:" />
-                <input type="text" placeholder="Alunos:" />
+            <h1 class="h1-sobre">CADASTRO</h1>
+            <div class="form-container-sobre">
+                <div class="input-group-sobre">
+                <input type="text" class="input-field" placeholder="Nome:">
+                <textarea class="textarea-field" placeholder="Sobre:"></textarea>
+                <input type="text" class="input-field" placeholder="Dia P">
+                <textarea class="textarea-field" placeholder="Sobre:"></textarea>
+                <input type="text" class="input-field" placeholder="Projeto XX">
                 </div>
-
-                <div class="profile-pic">
-                <img src="" alt="Ícone de usuário" />
+                <div class="input-group-sobre-big">
+                <input type="text" class="input-field" placeholder="Dia I">
+                <textarea class="textarea-field" placeholder="Sobre:"></textarea>
+                <input type="text" class="input-field" placeholder="Dia E">
+                <textarea class="textarea-field" placeholder="Sobre:"></textarea>
                 </div>
             </div>
-        </main>  -->
+        </main> 
     </div> 
   </body>
 </html>
