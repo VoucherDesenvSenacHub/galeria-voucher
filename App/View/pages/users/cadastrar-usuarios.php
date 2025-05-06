3edd1729096cbc5820f2bf838b8069d7a3767dc7
vik-cae <140553447+Tewdric@users.noreply.github.com>
2025-05-06 16:15:29 -0300

    Subindo as alterações

--- App/View/pages/users/cadastrar-usuarios.php
+++ App/View/pages/users/cadastrar-usuarios.php
@@ -22,71 +22,72 @@ require_once __DIR__ . "/../../componentes/head.php";
       <div class="form-container-users">
         <h1>CADASTRO</h1>
         <form class="form-dados">
-          <div class="form-group">
+          <div class="form-top">
+            <div class="form-group">
+              
+              <input type="text" placeholder=" Nome Completo" id="nome" name="nome">
+
+              <input type="email" placeholder=" Email" id="email" name="email">
+
+              <input type="url" id="linkedin" name="linkedin" placeholder=" Link do linkedin:">
+              
+              <input type="url" id="github" name="github" placeholder=" Link para o GitHub:">
             
-            <input type="text" placeholder=" Nome Completo" id="nome" name="nome">
-
-            <input type="email" placeholder=" Email" id="email" name="email">
-
-            <input type="url" id="linkedin" name="linkedin" placeholder=" Link do linkedin:">
-            
-            <input type="url" id="github" name="github" placeholder=" Link para o GitHub:">
-           
-            <input type="tel" id="telefone" name="telefone" placeholder=" Telefone:">
-          </div>
-
-
-          <div class="form-group-polo">
-            <input type="date" placeholder="Data de Nascimento" id="data-nascimento" name="data-nascimento" style="cursor: pointer;">
-
-            <select id="polo" name="polo" style="cursor: pointer;">
-              <option value="">Polo:</option>
-              <option value="polo1">Campo Grande</option>
-              <option value="polo2">Tres Lagoas</option>
-              <option value="polo2">Dourados</option>
-              <option value="polo2">Corumba</option>
-              <option value="polo2">Ponta Pora</option>
-            </select>
-
-            <select id="campo-turma" name="turma" style="cursor: pointer;">
-              <option value="">Turma:</option>
-              <option value="turma1">Turma 144</option>
-              <option value="turma2">Turma 145</option>
-              <option value="turma3">Turma 146</option>
-              <option value="turma4">Turma 147</option>
-              <option value="turma5">Turma 148 </option>
-            </select>
-
-          </div>
-
-          <div class="form-group-imagem">
-            <div class="input-file-cadastro">
-              <label class="input-file-wrapper">
-                <img id="preview" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>utilitarios/avatar.png" alt="Upload" />
-                <input type="file" id="fileInput" accept="image/*" style="display: none;" />
-              </label>
+              <input type="tel" id="telefone" name="telefone" placeholder=" Telefone:">
             </div>
 
-            <select id="campo-status" name="status" style="cursor: pointer;">
-              <option value="">Status:</option>
-              <option value="ativo">Ativo</option>
-              <option value="inativo">Inativo</option>
-            </select>
+
+            <div class="form-group-polo">
+              <input type="date" placeholder="Data de Nascimento" id="data-nascimento" name="data-nascimento" style="cursor: pointer;">
+
+              <select id="polo" name="polo" style="cursor: pointer;">
+                <option value="">Polo:</option>
+                <option value="polo1">Campo Grande</option>
+                <option value="polo2">Tres Lagoas</option>
+                <option value="polo2">Dourados</option>
+                <option value="polo2">Corumba</option>
+                <option value="polo2">Ponta Pora</option>
+              </select>
+
+              <select id="campo-turma" name="turma" style="cursor: pointer;">
+                <option value="">Turma:</option>
+                <option value="turma1">Turma 144</option>
+                <option value="turma2">Turma 145</option>
+                <option value="turma3">Turma 146</option>
+                <option value="turma4">Turma 147</option>
+                <option value="turma5">Turma 148 </option>
+              </select>
+
+            </div>
+
+            <div class="form-group-imagem">
+              <div class="input-file-cadastro">
+                <label class="input-file-wrapper">
+                  <img id="preview" src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>utilitarios/avatar.png" alt="Upload" />
+                  <input type="file" id="fileInput" accept="image/*" style="display: none;" />
+                </label>
+              </div>
+
+              <select id="campo-status" name="status" style="cursor: pointer;">
+                <option value="">Status:</option>
+                <option value="ativo">Ativo</option>
+                <option value="inativo">Inativo</option>
+              </select>
+            </div>
+          </div>
+          <div class="form-bottom">
+            <div class="form-group-about">
+              <textarea id="informacoes-adicionais" name="informacoes-adicionais" rows="15" cols="140" placeholder="Digite algo sobre o docente..." ></textarea>
+            </div>
+
+            <div class="form-group-buton">
+            <div>
+              <button type="button" id="btn-cancelar" >Cancelar</button>
+              <button type="submit" id="btn-cadastro" >Cadastrar</button>
+            </div>
           </div>
         </form>
-
-        <form>
-          <div class="form-group-about">
-            <textarea id="informacoes-adicionais" name="informacoes-adicionais" rows="15" cols="140" placeholder="Digite algo sobre o docente..." ></textarea>
-          </div>
-
-          <div class="form-group-buton">
-          <div>
-            <button type="button" id="btn-cancelar" >Cancelar</button>
-            <button type="submit" id="btn-cadastro" >Cadastrar</button>
-          </div>
-        </form>
-        
+                
       </div>
     </div>
   </main>
