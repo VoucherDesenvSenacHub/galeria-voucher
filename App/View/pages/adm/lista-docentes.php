6537a95abbb5c6b63e8bd0f4835fd305be43150e
Jaum_Vulcao <joaoheitoror0311@gmail.com>
2025-05-07 21:36:23 -0400

    commit
    sid e nav

--- App/View/pages/adm/lista-docentes.php
+++ App/View/pages/adm/lista-docentes.php
@@ -3,3 +3,7 @@ require_once __DIR__ . "/../../../Config/env.php";
 require_once __DIR__ . "/../../componentes/head.php";
 ?>
 
+<body>
+    <?php require_once __DIR__ . "/../../componentes/adm/sidebar.php"; ?>
+    <?php require_once __DIR__ . "/../../componentes/adm/nav.php"; ?>
+</body>
