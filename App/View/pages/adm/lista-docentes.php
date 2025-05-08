5adbf3af130b06b741746a0d0bef729fd16890ee
Jaum_Vulcao <joaoheitoror0311@gmail.com>
2025-05-08 19:28:06 -0400

    commit
    push

--- App/View/pages/adm/lista-docentes.php
+++ App/View/pages/adm/lista-docentes.php
@@ -6,4 +6,6 @@ require_once __DIR__ . "/../../componentes/head.php";
 <body>
     <?php require_once __DIR__ . "/../../componentes/adm/sidebar.php"; ?>
     <?php require_once __DIR__ . "/../../componentes/adm/nav.php"; ?>
+
+    <div class="form-group"><h3>lalalalalalalallala</h3>
 </body>
