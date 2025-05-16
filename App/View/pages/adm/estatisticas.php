4fbf9be306d27873244c6fc40ec2cc231d381c87
Matheus da Silva Corsine <matheuscorsine4129@gmail.com>
2025-05-15 21:33:42 -0400

    inicio da pagina de estatisticas

---
+++ App/View/pages/adm/estatisticas.php
@@ -0,0 +1,26 @@
+<?php 
+require_once __DIR__ . "/../../componentes/head.php";
+?>
+
+<body class="body-estatisticas">
+    <?php require_once __DIR__ . "/./../../componentes/adm/sidebar.php"; ?>
+    <?php require_once __DIR__ . "/./../../componentes/adm/nav.php"; ?>
+
+    <main class="conteudo-estatisticas">
+        <div>    
+            <div class="estatistica-profile">
+                <div class="user-icon">
+                    <div>
+                        <button id="estatistica-titulo">ESTATÍSTICA</button>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+
+            </div>
+
+        </div>
+    </main>
+</body>
+</html>
