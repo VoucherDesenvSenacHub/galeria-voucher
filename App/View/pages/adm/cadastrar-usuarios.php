4864431c225ed606fe9de278900e79b2b30140fd
Carlos Eduardo <carlosborges2007edu@gmail.com>
2025-05-28 20:07:05 -0400

    Atualiza estilos do formulário de cadastro de usuários, adiciona scrollbar personalizada e remove arquivo PHP obsoleto.

--- App/View/pages/adm/cadastrar-usuarios.php
+++ App/View/pages/adm/cadastrar-usuarios.php
@@ -1,8 +1,8 @@
 <?php 
 require_once __DIR__ . "/../../componentes/head.php";
-
+require_once __DIR__ . "/../../componentes/input.php";
+require_once __DIR__ . "/../../componentes/button.php";
 ?>
-
 
 <body class="body-cadastrar-users">
   <?php require_once __DIR__ . "/../../componentes/adm/sidebar.php"; ?>
@@ -11,36 +11,29 @@ require_once __DIR__ . "/../../componentes/head.php";
   <main class="conteudo-cadastro">
     <div class="user-profile">
       <div class="user-icon">
-      <button id="btn-docente">DOCENTE</button>
-      <button id="btn-aluno" >ALUNOS</button>
+        <button id="btn-docente" class="secondary-button">DOCENTE</button>
+        <button id="btn-aluno" class="secondary-button">ALUNOS</button>
       </div>
 
     </div>
 
-    
     <div class="container-users">
       <div class="form-container-users">
         <h1>CADASTRO</h1>
         <form class="form-dados">
           <div class="form-top">
             <div class="form-group">
-              
-              <input type="text" placeholder=" Nome Completo" id="nome" name="nome">
-
-              <input type="email" placeholder=" Email" id="email" name="email">
-
-              <input type="url" id="linkedin" name="linkedin" placeholder=" Link do linkedin:">
-              
-              <input type="url" id="github" name="github" placeholder=" Link para o GitHub:">
-            
-              <input type="tel" id="telefone" name="telefone" placeholder=" Telefone:">
+              <?php 
+                inputComponent('text', 'nome', 'Nome Completo');
+                inputComponent('text', 'email', 'Email');
+                inputComponent('text', 'linkedin', 'Link do linkedin');
+                inputComponent('text', 'github', 'Link para o GitHub');
+              ?>
             </div>
 
-
             <div class="form-group-polo">
-              <input type="date" placeholder="Data de Nascimento" id="data-nascimento" name="data-nascimento" style="cursor: pointer;">
-
-              <select id="polo" name="polo" style="cursor: pointer;">
+              <input type="date" class="input-text" placeholder="Data de Nascimento" id="data-nascimento" name="data-nascimento" style="cursor: pointer;">
+              <select id="polo" name="polo" class="input-text" style="cursor: pointer;">
                 <option value="">Polo:</option>
                 <option value="polo1">Campo Grande</option>
                 <option value="polo2">Tres Lagoas</option>
@@ -48,16 +41,14 @@ require_once __DIR__ . "/../../componentes/head.php";
                 <option value="polo2">Corumba</option>
                 <option value="polo2">Ponta Pora</option>
               </select>
-
-              <select id="campo-turma" name="turma" style="cursor: pointer;">
+              <select id="campo-turma" name="turma" class="input-text" style="cursor: pointer;">
                 <option value="">Turma:</option>
                 <option value="turma1">Turma 144</option>
                 <option value="turma2">Turma 145</option>
                 <option value="turma3">Turma 146</option>
                 <option value="turma4">Turma 147</option>
-                <option value="turma5">Turma 148 </option>
+                <option value="turma5">Turma 148</option>
               </select>
-
             </div>
 
             <div class="form-group-imagem">
@@ -67,30 +58,25 @@ require_once __DIR__ . "/../../componentes/head.php";
                   <input type="file" id="fileInput" accept="image/*" style="display: none;" />
                 </label>
               </div>
-
-              <select id="campo-status" name="status" style="cursor: pointer;">
-                <option value="">Status:</option>
-                <option value="ativo">Ativo</option>
-                <option value="inativo">Inativo</option>
-              </select>
             </div>
           </div>
           <div class="form-bottom">
             <div class="form-group-about">
-              <textarea id="informacoes-adicionais" name="informacoes-adicionais" rows="15" cols="140" placeholder="Digite algo sobre o docente..." ></textarea>
+              <textarea id="informacoes-adicionais" class="input-text" name="informacoes-adicionais" rows="15" cols="140" placeholder="Digite algo sobre o docente..." ></textarea>
             </div>
 
             <div class="form-group-buton">
-              <button type="button" id="btn-cancelar" >Cancelar</button>
-              <button type="submit" id="btn-cadastro" >Cadastrar</button>
+              <?php 
+                buttonComponent('secondary', 'Cancelar', false);
+                buttonComponent('primary', 'Cadastrar', true);
+              ?>
+            </div>
           </div>
         </form>
-                
       </div>
     </div>
   </main>
 
- 
   <script src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_JS'] . 'alunos/cadastrar_usuarios.js'; ?>"></script>
 </body>
 </html>
