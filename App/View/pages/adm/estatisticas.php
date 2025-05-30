f841c3d558057b60af487de416a22d7ec9252c00
Matheus da Silva Corsine <matheuscorsine4129@gmail.com>
2025-05-29 21:41:14 -0400

    adição titulo da página

--- App/View/pages/adm/estatisticas.php
+++ App/View/pages/adm/estatisticas.php
@@ -7,17 +7,18 @@ require_once __DIR__ . "/../../componentes/head.php";
     <?php require_once __DIR__ . "/./../../componentes/adm/nav.php"; ?>
 
     <main class="conteudo-estatisticas">
-        <div>    
+        <div>
+            <!-- profile (titulo da página) na parte de cima -->
             <div class="estatistica-profile">
                 <div class="user-icon">
                     <div>
-                        <button id="estatistica-titulo">ESTATÍSTICA</button>
+                        <button id="profile-titulo">ESTATÍSTICA</button>
                     </div>
                 </div>
             </div>
 
-            <div>
-
+            <div class="container">
+                <h1 class="titulo-estatistica">ATUALIZAR ESTATÍSTICAS</h1>
             </div>
 
         </div>
