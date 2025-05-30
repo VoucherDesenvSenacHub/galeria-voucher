5578a7abd2e91875a6d69334853415aca3015535
Carlos Eduardo <carlosborges2007edu@gmail.com>
2025-05-29 20:55:08 -0400

    refatoração do CSS e atualização da estrutura da página galeria-turma, incluindo a adição de classes específicas para melhor organização e responsividade

--- App/View/pages/users/galeria-turma.php
+++ App/View/pages/users/galeria-turma.php
@@ -3,28 +3,27 @@ require_once __DIR__ . "/../../../Config/env.php";
 require_once __DIR__ . "/../../componentes/head.php";
 ?>
 
-<body>
-    <header>
+<body class="galeria-turma-body">
+    <header class="galeria-turma-header">
         <?php require_once __DIR__ . "/./../../componentes/users/nav.php" ?>
         <?php require_once __DIR__ . "/./../../componentes/users/mira.php" ?>
     </header>
     <!-- parte das linhas -->
-    <section class="projeto">
-        <h1>Projetos da turma</h1>
+    <section class="galeria-turma-section galeria-turma-projeto">
+        <h1 class="galeria-turma-h1">Projetos da turma</h1>
     </section>
-    <section class="galeria">
+    <section class="galeria-turma-section galeria-turma-galeria">
         <h2>Galeria E-Commerce</h2>
-
     </section>
-    <section class="senac">
-        <h1>Senac Hub Academy</h1>
-        <h1>Campo Grande - MS</h1>
+    <section class="galeria-turma-section galeria-turma-senac">
+        <h1 class="galeria-turma-h1">Senac Hub Academy</h1>
+        <h1 class="galeria-turma-h1">Campo Grande - MS</h1>
     </section>
 
-    <section class="dia">
-        <img class="imagem-direita"
+    <section class="galeria-turma-section galeria-turma-dia">
+        <img class="galeria-turma-imagem-direita"
             src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma-galeria.png">
-        <div class="margin-top-left-turma-xx">
+        <div class="galeria-turma-margin-top-left-turma-xx">
             <h2>TURMA XX</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quod amet sint ut debitis optio
                 quaerat rerum qui soluta quibusdam suscipit temporibus, aliquam ducimus distinctio hic dolorem, corporis
@@ -32,22 +31,22 @@ require_once __DIR__ . "/../../componentes/head.php";
         </div>
     </section>
 
-    <section class="dia">
+    <div class="galeria-turma-binarynumber"></div>
+
+    <section class="galeria-turma-section galeria-turma-dia">
         <div>
             <h2>DIA I</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, blanditiis sequi, voluptas nam ut dicta
                 maxime dignissimos, est cupiditate esse iste aliquam tempora quos vero recusandae. Facilis, iste illo!
                 Unde.</p>
         </div>
-        <img class="imagem-diaI"
+        <img class="galeria-turma-imagem-diaI"
             src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma-galeria.png">
     </section>
 
-    <div class="binarynumber"></div>
-
-    <section class="dia">
+    <section class="galeria-turma-section galeria-turma-dia">
         <img src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma-galeria.png">
-        <div class="margin-top-left-dia-p">
+        <div class="galeria-turma-margin-top-left-dia-p">
             <h2>DIA P</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, esse. Cupiditate officia ut cum illo
                 doloremque quibusdam fuga natus necessitatibus veritatis eligendi facere, repellat aliquam eveniet aut
@@ -55,8 +54,8 @@ require_once __DIR__ . "/../../componentes/head.php";
         </div>
     </section>
 
-    <section class="dia">
-        <div class="margin-top-right-dia-e">
+    <section class="galeria-turma-section galeria-turma-dia">
+        <div class="galeria-turma-margin-top-right-dia-e">
             <h2>DIA E</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur nobis a laboriosam error nihil
                 placeat cum libero sapiente magni, voluptatum commodi accusamus repudiandae repellendus suscipit! Aut
@@ -65,40 +64,41 @@ require_once __DIR__ . "/../../componentes/head.php";
         <img src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma-galeria.png">
     </section>
 
-    <section class="dia">
+    <section class="galeria-turma-section galeria-turma-dia">
         <img src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>turmas/turma-galeria.png">
-        <div class="margin-top-left-projeto-xx">
+        <div class="galeria-turma-margin-top-left-projeto-xx">
             <h2>PROJETO XX</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In ad iusto omnis reiciendis dolor eos illo
                 quas. Ipsa modi amet officiis nulla veniam dolorem consequuntur pariatur minima. Neque, impedit
                 voluptatum!</p>
-            <div class="botaoprojeto"> <!-- <- classe que deixa o botao no lugar correto -->
-                <button class="primary-button" type="button">Ver Projeto</button>
+            <div class="galeria-turma-botaoprojeto">
+                <button class="galeria-turma-btn" type="button">Ver Projeto</button>
             </div>
         </div>
     </section>
 
-
-    <section class="cardss">
-        <h1>Alunos</h1>
+    <section class="galeria-turma-section galeria-turma-cardss">
+        <h1 class="galeria-turma-h1">Alunos</h1>
         <li>
-            <div class="container">
+            <div class="galeria-turma-container">
                 <?php for ($i = 0; $i <= 14; $i++) { ?>
                     <?php require __DIR__ . "/./../../componentes/users/card_desenvolvedores.php" ?>
                 <?php } ?>
             </div>
     </section>
 
-    <section class="cardss">
-        <h1>Professores</h1>
-        <div class="container">
+    <section class="galeria-turma-section galeria-turma-cardss">
+        <h1 class="galeria-turma-h1">Professores</h1>
+        <div class="galeria-turma-container">
             <?php for ($i = 0; $i <= 14; $i++) { ?>
                 <?php require __DIR__ . "/./../../componentes/users/card_desenvolvedores.php" ?>
             <?php } ?>
         </div>
-
     </section>
-    <?php require_once __DIR__ . "/./../../componentes/users/footer.php" //componente do rodapé ?>
+
+    <footer class="galeria-turma-footer">
+        <?php require_once __DIR__ . "/./../../componentes/users/footer.php" //componente do rodapé ?>
+    </footer>
 
 </body>
 
