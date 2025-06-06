04b79dde609b87e5375702e5c75f59cc4ed7c45f
Jaum_Vulcao <joaoheitoror0311@gmail.com>
2025-06-05 21:27:24 -0400

    commit
    commit froçado

--- App/View/pages/adm/lista-docentes.php
+++ App/View/pages/adm/lista-docentes.php
@@ -6,7 +6,7 @@ require_once __DIR__ . "/../../componentes/head.php";
   <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
 </head>
 
-<body>
+<body class="body-lista-docentes">
 
   <?php require_once __DIR__ . "/../../componentes/adm/sidebar.php"; ?>
   <?php require_once __DIR__ . "/../../componentes/adm/nav.php"; ?>
