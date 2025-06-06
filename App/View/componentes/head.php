6986d83a6b18cf18b20201776df077e2c3ab25d1
Gustavo Guimaraes <gustavo49914296@aluno.ms.senac.br>
2025-06-05 21:25:19 -0400

    implementação da função headerComponent para trocar o nome da página

--- App/View/componentes/head.php
+++ App/View/componentes/head.php
@@ -1,23 +1,23 @@
 <?php 
-require_once __DIR__ . '/./../../Config/env.php';
-
-function header($titulo){
-    echo "
-    <!DOCTYPE html>
-    <html lang='pt-br'>
+require_once __DIR__ . "/./../../Config/env.php";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
-    <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <link rel='preconnect' href='https://fonts.googleapis.com'>
-    <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
-    <link href='https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap' rel='stylesheet'>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
 
-    
 
-    <link rel='stylesheet' href='<?= VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_CSS'] ?>globals.css'>
 
+<link rel="stylesheet" href="<?= VARIAVEIS["APP_URL"] . VARIAVEIS["DIR_CSS"] ?>globals.css">
+<?php
+function headerComponent($titulo){
+    echo "
     <title>$titulo</title>
-</head>
-";
+    ";
 }
     ?>
+</head>
