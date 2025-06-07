73ba07867863441e9742b18aa1ccf47e8bb375ba
Xavier-sa <wellington222688@gmail.com>
2025-06-06 20:55:48 -0400

    feat:criacao tabelas

--- App/SQL/galeria_voucher_banco.sql
+++ App/SQL/galeria_voucher_banco.sql
@@ -4,3 +4,20 @@
 
  
 
+CREATE TABLE IF NOT EXISTS pessoa (
+    pessoa_id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    nome VARCHAR(255) NOT NULL,
+    linkedin VARCHAR(255),
+    github VARCHAR(255),
+    imagem_id INT, -- vincula com outra tabela!
+    descricao TEXT,
+    perfil ENUM('aluno', 'professor', 'adm') NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS usuario (
+    usuario_id INT AUTO_INCREMENT PRIMARY KEY,
+    pessoa_id INT NOT NULL,-- vincula com outra tabela!
+    senha VARCHAR(255) NOT NULL,
+    FOREIGN KEY (pessoa_id) REFERENCES pessoa(pessoa_id)
+);
