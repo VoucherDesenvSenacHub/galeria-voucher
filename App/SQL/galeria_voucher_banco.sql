d9df185528af7211d362312f538a701ba7fa7bce
Xavier-sa <wellington222688@gmail.com>
2025-06-06 21:32:42 -0400

    adicionando unsigned

--- App/SQL/galeria_voucher_banco.sql
+++ App/SQL/galeria_voucher_banco.sql
@@ -1,48 +1,52 @@
+-- 10 tabelas 
+
 CREATE DATABASE IF NOT EXISTS galeria_voucher;
+
+-- Drop database galeria_voucher;
 USE galeria_voucher;
 
 -- Tabela pessoa
 CREATE TABLE IF NOT EXISTS pessoa (
-    pessoa_id INT AUTO_INCREMENT PRIMARY KEY,
+    pessoa_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     nome VARCHAR(255) NOT NULL,
     linkedin VARCHAR(255),
     github VARCHAR(255),
-    imagem_id INT, -- pode ser uma futura FK se houver tabela de imagens
+    imagem_id INT, -- FK  tabela de imagens
     descricao TEXT,
     perfil ENUM('aluno', 'professor', 'adm') NOT NULL
 );
 
 -- Tabela usuario
 CREATE TABLE IF NOT EXISTS usuario (
-    usuario_id INT AUTO_INCREMENT PRIMARY KEY,
-    pessoa_id INT NOT NULL,
+    usuario_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    pessoa_id INT UNSIGNED NOT NULL,
     senha VARCHAR(255) NOT NULL,
     FOREIGN KEY (pessoa_id) REFERENCES pessoa(pessoa_id)
 );
 
 -- Tabela cidade
 CREATE TABLE IF NOT EXISTS cidade (
-    cidade_id INT AUTO_INCREMENT PRIMARY KEY,
+    cidade_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL
 );
 
 -- Tabela polo
 CREATE TABLE IF NOT EXISTS polo (
-    polo_id INT AUTO_INCREMENT PRIMARY KEY,
+    polo_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    cidade_id INT,
+    cidade_id INT UNSIGNED,-- FK tabela cidade
     FOREIGN KEY (cidade_id) REFERENCES cidade(cidade_id)
 );
 
 -- Tabela projeto
 CREATE TABLE IF NOT EXISTS projeto (
-    projeto_id INT AUTO_INCREMENT PRIMARY KEY,
+    projeto_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     descricao TEXT,
     data_inicio DATE,
     data_fim DATE,
-    imagem_id INT, -- FK para tabela de imagens
-    polo_id INT,
+    imagem_id INT UNSIGNED, -- FK para tabela 
+    polo_id INT UNSIGNED,--FK tabela polo
     FOREIGN KEY (polo_id) REFERENCES polo(polo_id)
 );
