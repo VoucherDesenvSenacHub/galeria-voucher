91db17fa07c84cc40a789624722c7ee484a75e76
matheuscorsine <matheuscorsine4129@gmail.com>
2025-06-23 11:38:23 -0400

    Adiciona scripts DDL e DML para a tabela de estatísticas

---
+++ App/SQL/dml/estatisticas.dml.sql
@@ -0,0 +1,5 @@
+-- insere a linha de dados com id=1, mas somente se ela ainda não existir
+-- isso evita criar duplicatas se você executar o script mais de uma vez
+INSERT INTO 'estatisticas' ('id', 'alunos', 'projetos', 'polos', 'horas')
+SELECT 1, 0, 0, 0, 0 FROM DUAL
+WHERE NOT EXISTS (SELECT 'id' FROM 'estatisticas' WHERE 'id' = 1);
