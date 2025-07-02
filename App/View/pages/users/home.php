e4fd06a759933c8bce693b647bf173136b6d4e73
jeipe <jp.2013barbosa@gmail.com>
2025-07-02 19:29:00 -0400

    update texto

--- App/View/pages/users/home.php
+++ App/View/pages/users/home.php
@@ -36,7 +36,8 @@ headerComponent('Página Inicial')
                     <h2>O QUE É ?</h2>
                     <p>
                         Qualifique-se para uma das áreas mais promissoras da atualidade com o Programa Voucher
-                        Desenvolvedor. Oferecemos vagas gratuitas para o curso Técnico em Desenvolvimento de Sistemas,
+                        Desenvolvedor.
+                        Oferecemos vagas gratuitas para o curso Técnico em Desenvolvimento de Sistemas,
                         com carga horária de 1.200 horas. Beneficie-se de uma experiência prática com interação direta
                         com empresas de tecnologia e, a partir do sexto mês, tenha a chance de conseguir um estágio
                         remunerado e se destacar no curso. Invista no seu futuro e abra portas para uma carreira de
@@ -48,8 +49,13 @@ headerComponent('Página Inicial')
                     <h2>PARA QUEM É ?</h2>
                     <p>
                         Podem participar do processo seletivo pessoas com renda per capita familiar de até 2 salários
-                        mínimos. São elegíveis estudantes do ensino médio (1º, 2º ou 3º ano) matriculados em
+                        mínimos.
+                    </p>
+                    <p>
+                        São elegíveis estudantes do ensino médio (1º, 2º ou 3º ano) matriculados em
                         instituições públicas de ensino em Mato Grosso do Sul, desde que estejam, no mínimo, no 2º ano.
+                    </p>
+                    <p>
                         Também podem participar aqueles que estão concluindo ou já concluíram o ensino médio, em
                         instituições públicas ou privadas.
                     </p>
