2442f77460ebeab83f4a5f3ed8c312f98ff1cfe4
W.Xavier <wellington222688@gmail.com>
2025-07-04 21:36:39 -0400

    alteracoes css , sobre , cadastroTurmas

--- App/View/pages/adm/cadastroTurmas/sobre.php
+++ App/View/pages/adm/cadastroTurmas/sobre.php
@@ -38,38 +38,6 @@ require_once __DIR__ . "/../../../componentes/head.php";
         </a>
 
        
-        <div class="form-container-sobre">
-          <div class="input-grupos">
-
-            <div class="input-group-sobre">
-              <?php inputComponent("Nome:", "Text", "Nome"); ?>
-              <textarea class="textarea-field" placeholder="Sobre:"></textarea>
-
-              <?php inputComponent("Dia P", "text", "Dia_p"); ?>
-              <textarea class="textarea-field" placeholder="Sobre:"></textarea>
-
-              <?php inputComponent("Projeto XX", "text", "Projeto_xx"); ?>
-              <textarea class="textarea-field" placeholder="Sobre:"></textarea>
-            <div class="topo-lista-alunos">
-            <?php buttonComponent('primary', 'NOVO PROJETO', false, VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_ADM'] . 'cadastroTurmas/sobre.php'); ?>
-            </div>
-
-            <div class="input-group-sobre">
-              <?php inputComponent("Dia I", "text", " Dia_i"); ?>
-              <textarea class="textarea-field" placeholder="Sobre:"></textarea>
-
-              <?php inputComponent("Dia E", "text", "Dia_e"); ?>
-              <textarea class="textarea-field" placeholder="Sobre:"></textarea>
-            </div>
-
-            <div class="link-projeto">
-              <?php inputComponent("Link de Projeto:", "text", "Link_projeto"); ?>
-              <div class="btn-novos-projeto">
-                <a href="imagens.php">
-                  <button class="componente-botao btn-imagens">NOVOS PROJETOS</button>
-                </a>
-              </div>
-            </div>
 
           </div>
           
