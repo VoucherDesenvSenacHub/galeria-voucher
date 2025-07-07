fadca2e9b60150122cd45cf51790dcab7bc383a7
Xavier-sa <wellington222688@gmail.com>
2025-07-07 11:42:24 -0300

    alterei de link para dontpad para form do google

--- App/View/componentes/users/desenvolvedores.php
+++ App/View/componentes/users/desenvolvedores.php
@@ -11,63 +11,63 @@ $desenvolvedores = [
         'nome' => 'José Otávio',
         'funcao' => 'Desenvolvedor',    
         'github' => 'https://github.com/OtavioDayrots',
-        'linkedin' => 'https://dontpad.com/turma146dados',
+        'linkedin' => 'https://forms.gle/xsEzD6xCHFagFu3k6',
         'foto' => 'https://avatars.githubusercontent.com/u/152044189?v=4'
     ],
     [
         'nome' => 'Luiz Oliveira',
         'funcao' => 'Desenvolvedora',
         'github' => '',
-        'linkedin' => 'https://dontpad.com/turma146dados',
+        'linkedin' => 'https://forms.gle/xsEzD6xCHFagFu3k6',
         'foto' => 'https://avatars.githubusercontent.com/u/118494854?v=4'   
     ],
     [
         'nome' => 'Jonatan Samuel',
         'funcao' => 'Desenvolvedor',
         'github' => 'https://github.com/samuelserri',
-        'linkedin' => 'https://dontpad.com/turma146dados',
+        'linkedin' => 'https://forms.gle/xsEzD6xCHFagFu3k6',
         'foto' => 'https://avatars.githubusercontent.com/u/172449361?v=4'
     ],
     [
         'nome' => 'Anuar El',
         'funcao' => 'Desenvolvedor',
         'github' => 'https://github.com/AnuarRezz',
-        'linkedin' => 'https://dontpad.com/turma146dados', 
+        'linkedin' => 'https://forms.gle/xsEzD6xCHFagFu3k6', 
         'foto' => 'https://avatars.githubusercontent.com/u/172449471?v=4'
     ],
     [
         'nome' => 'Rodrigo Santos',
         'funcao' => 'Desenvolvedor',
         'github' => 'https://github.com/rodrigo570282',
-        'linkedin' => 'https://dontpad.com/turma146dados',  
+        'linkedin' => 'https://forms.gle/xsEzD6xCHFagFu3k6',  
         'foto' => 'https://avatars.githubusercontent.com/u/172452119?v=4'
     ],  
     [
         'nome' => 'Saambrc',
         'funcao' => 'Desenvolvedor',
         'github' => 'https://github.com/Saambrc',
-        'linkedin' => 'https://dontpad.com/turma146dados',  
+        'linkedin' => 'https://forms.gle/xsEzD6xCHFagFu3k6',  
         'foto' => 'https://avatars.githubusercontent.com/u/172451323?v=4'
     ],
     [   
         'nome' => 'Matheus Corsine',
         'funcao' => 'Desenvolvedor',
         'github' => 'https://github.com/matheuscorsine',    
-        'linkedin' => 'https://dontpad.com/turma146dados',  
+        'linkedin' => 'https://forms.gle/xsEzD6xCHFagFu3k6',  
         'foto' => 'https://avatars.githubusercontent.com/u/172449520?v=4'   
     ],
     [
         'nome' => 'LucasAjpert',
         'funcao' => 'Desenvolvedor',
         'github' => 'https://github.com/LucasAjpert',
-        'linkedin' => 'https://dontpad.com/turma146dados',  
+        'linkedin' => 'https://forms.gle/xsEzD6xCHFagFu3k6',  
         'foto' => 'https://avatars.githubusercontent.com/u/173213330?v=4'
     ],
     [
         'nome' => 'Henrique Guisa',
         'funcao' => 'Desenvolvedor',
         'github' => 'https://github.com/henriguisatec',
-        'linkedin' => 'https://dontpad.com/turma146dados',  
+        'linkedin' => 'https://forms.gle/xsEzD6xCHFagFu3k6',  
         'foto' => 'https://avatars.githubusercontent.com/u/172449651?v=4'
     ]   
 ];
