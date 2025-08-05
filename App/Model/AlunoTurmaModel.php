ef583841608fc8bff5c441ecc3b50b1b1c599f46
jeipe <jp.2013barbosa@gmail.com>
2025-08-04 21:22:32 -0400

    model e sql
    atualizaçoes no model

--- App/Model/AlunoTurmaModel.php
+++ App/Model/AlunoTurmaModel.php
@@ -4,5 +4,19 @@ require_once __DIR__ . "/BaseModel.php";
 
 class AlunoTurmaModel extends BaseModel 
 {
-    
+    public function __construct()
+    {
+        $this->tabela = "turma";
+        parent::__construct();
+    }
+
+    public function lista()
+    {
+        $query = "SELECT * FROM $this->tabela";
+
+        $stmt = $this->pdo->prepare($query);
+
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 require_once __DIR__ . '/App/Config/env.php';
+require_once __DIR__ . '/app/Model/AlunoTurmaModel.php';
 
-header('Location: ' . VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_USER'] . 'home.php');
+$alunoTurma = new AlunoTurmaModel();
 
+
+
+var_dump($alunoTurma->lista());
