ac4ca037ae0d9f2e7c52fb1a4354cc10525dc686
Rodrigo Santos <rodrigorrs.sp@gmail.com>
2025-08-06 19:45:07 -0400

    060825

--- App/SQL/ddl/vincular.sql
+++ App/SQL/ddl/vincular.sql
@@ -1,20 +1,39 @@
--- vincular alunos a uma turma
+-- exibir todos os alunos de uma turma
 
-select nome, data_matricula -- o que voce quer da tabela
+select p.nome, t.nome 
 from turma t -- a tabela que vc quer ( a letra a frente é um apelido)
 inner join aluno_turma at on at.turma_id = t.turma_id 
 inner join pessoa p on p.pessoa_id = at.pessoa_id 
+where t.turma_id =1;
 
-select *
+-- exibir todos os docentes de uma turma
+select p.nome, t.nome
 from turma t 
-where t.turma_id =3; 
+inner join docente_turma dt on dt.turma_id = t.turma_id 
+inner join pessoa p on p.pessoa_id =dt.pessoa_id 
+where t.turma_id =1;
 
-select linkedin, nome 
-from pessoa p
-where p.pessoa_id =10;
 
 -- inserir aluno a uma turma
 
 insert into aluno_turma 
 (turma_id, pessoa_id,data_matricula)
 values (1,15,'2024-01-25');
+
+-- inserir docente a uma turma
+insert into docente_turma 
+(docente_turma_id,pessoa_id,turma_id,data_associacao)
+values (7,5,1,'2024-02-01');
+
+
+
+ -- exibir dados da turma
+ select *
+from turma t 
+where t.turma_id =3; 
+
+
+-- exibir dados da pessoa
+select linkedin, nome 
+from pessoa p
+where p.pessoa_id =10;
