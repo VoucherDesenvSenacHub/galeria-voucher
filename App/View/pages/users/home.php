178fd672c41cd8293ab94859eea3c93c372c7290
Bruno <brunoribeiro.cgms@gmail.com>
2025-08-06 20:01:18 -0400

    modificação home.php

--- App/View/pages/users/home.php
+++ App/View/pages/users/home.php
@@ -30,10 +30,77 @@ try {
     <?php require_once __DIR__ . "/../../componentes/users/mira.php"; ?>
 
     <main class="main-user">
-        <section id="secao1">
-            </section>
+    <section id="secao1">
+            <div class="content">
+                <div class="numero">
+                    <img src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>utilitarios/numeros.png"
+                        alt="Números">
+                </div>
+
+                <div class="nome-voucher">
+                    <img src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>utilitarios/nome.png"
+                        alt="Voucher Desenvolvedor">
+                </div>
+
+                <div class="mapa">
+                    <img src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>utilitarios/mapa.png"
+                        alt="Mapa do Brasil">
+                </div>
+            </div>
+        </section>
+
         <section id="secao2">
-            </section>
+             <div class="container">
+                <div class="card">
+                    <h2>O QUE É ?</h2>
+                    <p>
+                        Qualifique-se para uma das áreas mais promissoras da atualidade com o Programa Voucher
+                        Desenvolvedor.
+                        Oferecemos vagas gratuitas para o curso Técnico em Desenvolvimento de Sistemas,
+                        com carga horária de 1.200 horas.
+                    </p>
+                    <p>
+                        Beneficie-se de uma experiência prática com interação direta
+                        com empresas de tecnologia e, a partir do sexto mês, tenha a chance de conseguir um estágio
+                        remunerado e se destacar no curso. Invista no seu futuro e abra portas para uma carreira de
+                        sucesso em TI.
+                    </p>
+                </div>
+
+                <div class="card">
+                    <h2>PARA QUEM É ?</h2>
+                    <p>
+                        Podem participar do processo seletivo pessoas com renda per capita familiar de até 2 salários
+                        mínimos.
+                    </p>
+                    <p>
+                        São elegíveis estudantes do ensino médio (1º, 2º ou 3º ano) matriculados em
+                        instituições públicas de ensino em Mato Grosso do Sul, desde que estejam, no mínimo, no 2º ano.
+                    </p>
+                    <p>
+                        Também podem participar aqueles que estão concluindo ou já concluíram o ensino médio, em
+                        instituições públicas ou privadas.
+                    </p>
+                </div>
+
+                <div class="card">
+                    <h2>POR QUE FAZER ?</h2>
+                    <p>
+                        A área de Tecnologia da Informação está em expansão, com uma alta demanda por profissionais de
+                        Desenvolvimento de Sistemas.
+                    </p>
+                    <p>
+                        Especializar-se nessa área oferece maior empregabilidade, inclusive
+                        internacional, uma boa remuneração (a média é de R$ 2.221,00), e a possibilidade de novas
+                        carreiras e desenvolvimento.
+                    </p>
+                    <p>
+                        É um setor em constante crescimento, apresentando excelentes
+                        perspectivas para quem busca se destacar.
+                    </p>
+                </div>
+            </div>
+        </section>
         <section id="secao3">
              <div class="container2">
                 <h1>SUA EVOLUÇÃO COMEÇA AQUI</h1>
