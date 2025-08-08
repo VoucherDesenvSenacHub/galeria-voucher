d809c82273da27b5c4d2ce007968e931ecf2e689
Matheus da Silva Corsine <matheuscorsine4129@gmail.com>
2025-08-07 21:41:00 -0400

    Adição elementos "logotipo" .svg para página home.php e alterações na mira

--- App/View/componentes/users/mira.php
+++ App/View/componentes/users/mira.php
@@ -2,8 +2,4 @@
     <div class="mira"></div>
     <div class="mira1"></div>
     <div class="mira2"></div>
-    <div class="nome-voucher">
-        <img src="<?php echo VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_IMG'] ?>utilitarios/nome.png"
-            alt="Voucher Desenvolvedor">
-    </div>
 </div>
