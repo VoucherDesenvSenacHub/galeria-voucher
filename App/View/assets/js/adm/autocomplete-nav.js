25e22d0fd8395a40e31c451e2400e25a621b1e1f
vik-cae <140553447+Tewdric@users.noreply.github.com>
2025-08-08 19:32:10 -0300

    Feature: Autocomplete para o nav e requisição com json do banco de dados

---
+++ App/View/assets/js/adm/autocomplete-nav.js
@@ -0,0 +1,42 @@
+async function buscarAlunos() {
+    console.log('buscarAlunos chamada');
+    const response = await fetch('/galeria-voucher/app/Controls/AlunoController.php?acao=alunos');
+    const dados = await response.json();
+    console.log(await dados)
+    return dados;
+}
+// Função para ativar autocomplete no form dentro do modal
+async function ativarAutocomplete() {
+    const pessoas = await buscarAlunos();
+
+    const input = document.getElementById("pesquisar-pessoa");
+    if (!input) return; // só executa se input existir (modal aberto)
+
+    const sugestoes = document.getElementById("sugestoes");
+    const selecionados = document.getElementById("pessoas-selecionadas");
+    const adicionados = new Set();
+
+    input.addEventListener("input", () => {
+
+        const valor = input.value.toLowerCase();
+        sugestoes.innerHTML = "";
+        if (valor.length === 0) return;
+
+        pessoas.forEach(pessoa => {
+            const nome = pessoa.nome_pessoa.toLowerCase();
+            if (nome.startsWith(valor) && !adicionados.has(nome)) {
+                const div = document.createElement("div");
+                div.textContent = pessoa.nome_pessoa;        // mostrar nome_pessoa
+                div.style.cursor = "pointer";
+                div.onclick = () => {
+                    const url = `/galeria-voucher/App/View/pages/users/galeria-turma.php?nome=${encodeURIComponent(pessoa.nome_pessoa)}`;
+                    window.location.href = url;
+                };
+                sugestoes.appendChild(div);
+            }
+        });
+    });
+
+}
+
+ativarAutocomplete()
