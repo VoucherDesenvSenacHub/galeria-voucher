d368f69f970ee87dae709a1a5c7b053ef6cdf1cb
Luís Cunha <luisf.cunha05@gmail.com>
2025-09-09 19:55:05 -0400

    :bug: fix(service): Correção import em AuthService

--- App/Service/AuthService.php
+++ App/Service/AuthService.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../../Controller/UsuarioController.php';
+require_once __DIR__ . '/../Controller/UsuarioController.php';
 
 $usuarioController = new UsuarioController();
 
