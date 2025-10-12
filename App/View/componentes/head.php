a39a761e0764e916fdcfb1dace7dd3b74ab99189
Lucas Ajpert <lucasajpertengenharia@gmail.com>
2025-10-12 12:24:29 -0300

    easter egg

--- App/View/componentes/head.php
+++ App/View/componentes/head.php
@@ -22,6 +22,8 @@ require_once __DIR__ . '/button.php';
 <link rel="stylesheet" href="<?= VARIAVEIS["APP_URL"] . VARIAVEIS["DIR_CSS"] ?>globals.css">
 <link rel="stylesheet" href="<?= VARIAVEIS["APP_URL"] . VARIAVEIS["DIR_CSS"] ?>adm/modal-cadastro.css">
 
+<link rel="stylesheet" href="<?= VARIAVEIS["APP_URL"] . VARIAVEIS["DIR_CSS"] ?>easter_egg.css">
+
 <!-- Script para controle do menu hambúrguer -->
 <script src="<?= VARIAVEIS["APP_URL"] . VARIAVEIS["DIR_JS"] ?>users/nav.js"></script>
 <script src="<?= VARIAVEIS["APP_URL"] . VARIAVEIS["DIR_JS"] ?>global.js"></script>
