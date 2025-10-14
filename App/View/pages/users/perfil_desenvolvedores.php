ea64e67194b21425ac483ca476351f22ab6682d3
Luís Cunha <luisf.cunha05@gmail.com>
2025-10-14 19:56:36 -0400

    :bug: fix: correção layout página desenvolvedores

--- App/View/pages/users/perfil_desenvolvedores.php
+++ App/View/pages/users/perfil_desenvolvedores.php
@@ -12,28 +12,31 @@ headerComponent('Desenvolvedores');
 ?>
 
 <body class="layout body_dev">
-    <header class="header_dev">
-        <?php
-        // Aqui você passa a função JS no parâmetro do botão
-        buttonComponent('primary', 'VOLTAR', false, "javascript:window.history.back()");
-        ?>
-        <div class="titulo-pagina">
-            <h1 class="titulodev">DESENVOLVEDORES</h1>
-        </div>
-    </header>
-    <main class="main_dev">
-        <?php require_once __DIR__ . "/../../componentes/users/mira.php"; ?>
-
-        <section class="galeria-turma-cardss">
-            <h1 class="galeria-turma-h1">Alunos</h1>
-            <div class="galeria-turma-container">
-                <?php foreach ($pessoasTurma as $pessoa): ?>
-                    <?php include __DIR__ . "/../../componentes/users/card_pessoa.php"; ?>
-                <?php endforeach; ?>
+    <section class="main-section layout-main">
+        <header class="nav-head header_dev">
+            <?php
+            // Aqui você passa a função JS no parâmetro do botão
+            buttonComponent('primary', 'VOLTAR', false, "javascript:window.history.back()", null, null, "dev-no-button");
+            ?>
+            <div class="titulo-pagina">
+                <h1 class="titulodev">DESENVOLVEDORES</h1>
             </div>
-        </section>
-
+        </header>
+        <main class="main_dev">
+            <?php require_once __DIR__ . "/../../componentes/users/mira.php"; ?>
+    
+            <section class="galeria-turma-cardss">
+                <h1 class="galeria-turma-h1">Alunos</h1>
+                <div class="galeria-turma-container">
+                    <?php foreach ($pessoasTurma as $pessoa): ?>
+                        <?php include __DIR__ . "/../../componentes/users/card_pessoa.php"; ?>
+                    <?php endforeach; ?>
+                </div>
+            </section>
+    
+        </main>
         <?php require_once __DIR__ . "/./../../componentes/users/footer.php" ?>
-    </main>
+
+    </section>
 </body>
 </html>
