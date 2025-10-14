b5316d2f441c05af9128750698066a9d809bd6b6
brunoDevfull <brunoribeiro.cgms@gmail.com>
2025-10-14 19:44:51 -0400

    App/View/componentes/users/footer.php View Atualizada para usar Config::get() em vez de VARIAVEIS.

--- App/View/componentes/users/footer.php
+++ App/View/componentes/users/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="row">
         <div class="col-100">
-            <a href="<?= VARIAVEIS['APP_URL'] . VARIAVEIS['DIR_USER'] ?>perfil_desenvolvedores.php">&copy;2025 VOUCHER DESENVOLVEDOR 146</a>
+            <a href="<?= Config::get('APP_URL') . Config::get('DIR_USER') ?>perfil_desenvolvedores.php">&copy;2025 VOUCHER DESENVOLVEDOR 146</a>
         </div>
     </div>
 </footer>
