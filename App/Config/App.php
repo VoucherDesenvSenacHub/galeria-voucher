d309e745e1db607a9435dc898819f5d2de61bd0f
LuizzOliveira <lhso1991@gmail.com>
2025-10-29 02:53:28 -0300

    fix: Refatorando class config.

--- App/Config/App.php
+++ App/Config/App.php
@@ -22,4 +22,59 @@ class Config
     {
         return self::$settings[$key] ?? null;
     }
+
+    public static function getAppUrl()
+    {
+        return self::get('APP_URL');
+    }
+
+    
+    public static function getDirImg()
+    {
+        return self::getAppUrl() . self::get('DIR_IMG');
+    }
+
+    public static function getDirCss()
+    {
+        return self::getAppUrl() . self::get('DIR_CSS');
+    }
+
+    public static function getDirJs()
+    {
+        return self::getAppUrl() . self::get('DIR_JS');
+    }
+
+    public static function getDirAdm()
+    {
+        return self::getAppUrl() . self::get('DIR_ADM');
+    }
+
+    public static function getDirUser()
+    {
+        return self::getAppUrl() . self::get('DIR_USER');
+    }
+
+    public static function getDirLogout()
+    {
+        return self::getAppUrl() . self::get('DIR_LOGOUT');
+    }
+
+    //     /**
+    //  * Redireciona o usuário para uma URL.
+    //  *
+    //  * @param string $path O caminho para o qual redirecionar.
+    //  * @param array $params Parâmetros GET a serem adicionados à URL.
+    //  */
+    // public static function to(string $path, array $params = []): void
+    // {
+    //     $url = self::getAppUrl() . $path;
+
+    //     if (!empty($params)) {
+    //         $url .= '?' . http_build_query($params);
+    //     }
+
+    //     header("Location: " . $url);
+    //     exit;
+    // }
+
 }
