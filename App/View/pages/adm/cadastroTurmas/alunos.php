ef67e19a3ceed226baf2e6290e2ac2b233918034
José Otávio Alves <joseotavioalves321@gmail.com>
2025-11-05 21:02:58 -0400

    refactor: update require paths for BreadCrumbs component to use consistent casing

--- App/View/pages/adm/cadastroTurmas/alunos.php
+++ App/View/pages/adm/cadastroTurmas/alunos.php
@@ -6,7 +6,7 @@ require_once __DIR__ . "/../../../componentes/head.php";
 require_once __DIR__ . "/../../../../Service/AuthService.php";
 require_once __DIR__ . "/../../../../Model/AlunoModel.php";
 require_once __DIR__ . "/../../../componentes/adm/tabsTurma.php";
-require_once __DIR__ . "/../../../componentes/breadCrumbs.php";
+require_once __DIR__ . "/../../../componentes/BreadCrumbs.php";
 
 $turmaId = Request::getId("turma_id");
 if (!$turmaId) {
