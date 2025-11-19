37ca92a2238e70fa9298706a1211ba46c32a213d
José Otávio Alves <joseotavioalves321@gmail.com>
2025-11-18 21:20:35 -0400

    refatorar: melhorar a seleção de elementos do modal e as verificações de nulo em desvincula-aluno.js e desvincula-docente.js

--- App/View/assets/js/adm/desvincula-docente.js
+++ App/View/assets/js/adm/desvincula-docente.js
@@ -1,13 +1,14 @@
 function confirmarDesvinculacao(pessoaId, turmaId,  nomeDocente ) {  
     const modalProfessorRemover = document.getElementById("modal-desvincular-docente")  
-    const inputDocenteId = document.querySelector("input[name='pessoa_id']")
-    const inputTurmaId = document.querySelector("input[name='turma_id']")
+    if (!modalProfessorRemover) return;
+    
+    const inputDocenteId = modalProfessorRemover.querySelector("input[name='pessoa_id']")
+    const inputTurmaId = modalProfessorRemover.querySelector("input[name='turma_id']")
     const nomeDocenteinsert = document.getElementById("docente-confirmacao")    
 
-
-    nomeDocenteinsert.innerHTML = nomeDocente
-    inputTurmaId.value =  turmaId
-    inputDocenteId.value = pessoaId
+    if (nomeDocenteinsert) nomeDocenteinsert.innerHTML = nomeDocente
+    if (inputTurmaId) inputTurmaId.value = turmaId
+    if (inputDocenteId) inputDocenteId.value = pessoaId
     modalProfessorRemover.style.display = "flex";
 }
 
